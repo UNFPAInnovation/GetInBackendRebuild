85198c14672a0e63fb3c977ba199af45e3caee8c
codephillip <codephillip@gmail.com>
2019-11-21 16:21:05 +0300

    disabled filtering appointments by user

--- app/views.py
+++ app/views.py
@@ -210,21 +210,22 @@ class AppointmentEncounterView(ListCreateAPIView):
 class AppointmentView(ListCreateAPIView):
     def get_queryset(self):
         user = self.request.user
-        if user.role == USER_TYPE_MIDWIFE:
-            # return all appointment from CHEWS attached to the midwife or
-            # any appointment created by the midwife herself
-            users = User.objects.filter(midwife=user)
-            appointments = Appointment.objects.filter(
-                Q(user_id__in=[user.id for user in users]) | Q(user__id=user.id)).order_by('-created_at')
-        elif user.role in [USER_TYPE_CHEW]:
-            # return appointments created by CHEW
-            appointments = Appointment.objects.filter(Q(user=user) | Q(girl__user=user)).order_by('-created_at')
-        elif user.role in [USER_TYPE_DHO]:
-            # return all appointments in the DHO district
-            appointments = Appointment.objects.filter(user__district=user.district).order_by('-created_at')
-        else:
-            # return everything for super users and developers
-            appointments = Appointment.objects.all().order_by('-created_at')
+        # if user.role == USER_TYPE_MIDWIFE:
+        #     # return all appointment from CHEWS attached to the midwife or
+        #     # any appointment created by the midwife herself
+        #     users = User.objects.filter(midwife=user)
+        #     appointments = Appointment.objects.filter(
+        #         Q(user_id__in=[user.id for user in users]) | Q(user__id=user.id)).order_by('-created_at')
+        # elif user.role in [USER_TYPE_CHEW]:
+        #     # return appointments created by CHEW
+        #     appointments = Appointment.objects.filter(Q(user=user) | Q(girl__user=user)).order_by('-created_at')
+        # elif user.role in [USER_TYPE_DHO]:
+        #     # return all appointments in the DHO district
+        #     appointments = Appointment.objects.filter(user__district=user.district).order_by('-created_at')
+        # else:
+        #     # return everything for super users and developers
+        #     appointments = Appointment.objects.all().order_by('-created_at')
+        appointments = Appointment.objects.all().order_by('-created_at')
         return appointments
 
     permission_classes = (IsAuthenticated, DRYPermissions)
