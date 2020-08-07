608ce8ce5df43fac0b4e1315f480220da7f40931
codephillip <codephillip@gmail.com>
2020-08-07 15:45:28 +0300

    added midwife follow up tests

--- app/tests/__init__.py
+++ app/tests/__init__.py
@@ -1,4 +1,7 @@
+# Add these environment variables
+# PYTHONUNBUFFERED=1;DJANGO_SETTINGS_MODULE=GetInBackendRebuild.settings;PASSWORD=Doppler25;DJANGO_DATABASE=unittest;AFRICAS_TALKING_ENV=test
 from app.tests.usertests import *
 from app.tests.midwifemappingtest import *
 from app.tests.chewmappingtest import *
 from app.tests.chewfollowuptest import *
+from app.tests.midwifefollowuptest import *
