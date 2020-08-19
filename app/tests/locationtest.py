156a6a63aef840d98dd65a65560a001e971b54d1
codephillip <codephillip@gmail.com>
2020-08-19 21:24:43 +0300

    updated location tests

--- app/tests/locationtest.py
+++ app/tests/locationtest.py
@@ -6,36 +6,36 @@ from app.tests.parenttest import ParentTest
 
 class TestLocation(ParentTest):
     def test_locations(self):
-        self.assertEqual(Region.objects.count(), 1)
-        self.assertEqual(District.objects.count(), 1)
-        self.assertEqual(County.objects.count(), 1)
-        self.assertEqual(SubCounty.objects.count(), 1)
-        self.assertEqual(Parish.objects.count(), 1)
-        self.assertEqual(Village.objects.count(), 1)
+        self.assertEqual(Region.objects.count(), 2)
+        self.assertEqual(District.objects.count(), 2)
+        self.assertEqual(County.objects.count(), 2)
+        self.assertEqual(SubCounty.objects.count(), 2)
+        self.assertEqual(Parish.objects.count(), 2)
+        self.assertEqual(Village.objects.count(), 2)
 
     def test_location_endpoints(self):
         self.client.force_authenticate(user=self.user)
         url = reverse("districts")
         request = self.client.get(url)
         self.assertEqual(request.status_code, status.HTTP_200_OK)
-        self.assertEqual(request.json()['count'], 1)
+        self.assertEqual(request.json()['count'], 2)
 
         url = reverse("counties")
         request = self.client.get(url)
         self.assertEqual(request.status_code, status.HTTP_200_OK)
-        self.assertEqual(request.json()['count'], 1)
+        self.assertEqual(request.json()['count'], 2)
 
         url = reverse("subcounties")
         request = self.client.get(url)
         self.assertEqual(request.status_code, status.HTTP_200_OK)
-        self.assertEqual(request.json()['count'], 1)
+        self.assertEqual(request.json()['count'], 2)
 
         url = reverse("parishes")
         request = self.client.get(url)
         self.assertEqual(request.status_code, status.HTTP_200_OK)
-        self.assertEqual(request.json()['count'], 1)
+        self.assertEqual(request.json()['count'], 2)
 
         url = reverse("villages")
         request = self.client.get(url)
         self.assertEqual(request.status_code, status.HTTP_200_OK)
-        self.assertEqual(request.json()['count'], 1)
+        self.assertEqual(request.json()['count'], 2)
