34fd4cecf3e554d66976076b58bb096df646e3d5
codephillip <codephillip@gmail.com>
2021-01-06 20:21:26 +0300

    disabled day after appointment and actual day sms notifications

--- app/cron.py
+++ app/cron.py
@@ -11,8 +11,8 @@ def notifier_appointment_reminder_cron_job():
     """
     notifier = NotifierView()
     notifier.send_appointment_three_days_before_date()
-    notifier.send_appointment_one_day_after_date()
-    notifier.send_appointment_on_actual_day()
+    # notifier.send_appointment_one_day_after_date()
+    # notifier.send_appointment_on_actual_day()
 
 
 def notifier_daily_usage_reminder_cron_job():
