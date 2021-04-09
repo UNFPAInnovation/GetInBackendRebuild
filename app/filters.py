33ec8ac44c2fed4d0d567a978a75b96cbd948173
codephillip <codephillip@gmail.com>
2021-04-09 13:19:42 +0300

    fixed created_to date filter by incrementing by one day

--- app/filters.py
+++ app/filters.py
@@ -1,3 +1,5 @@
+import datetime
+
 import django_filters
 
 from app.models import Girl, FollowUp, Appointment, MappingEncounter, Delivery, User, AppointmentEncounter
@@ -7,13 +9,17 @@ class SuperFilter(django_filters.FilterSet):
     created_from = django_filters.DateTimeFilter(field_name='created_at', lookup_expr='gte',
                                                  help_text='Created at is greater than or equal to')
     created_to = django_filters.DateTimeFilter(field_name='created_at', lookup_expr='lte',
-                                               help_text='Created is less than or equal to')
+                                               help_text='Created is less than or equal to', method='to_filter')
 
     class Meta:
         model = Girl
         fields = {
             'created_from', 'created_to'
         }
+
+    def to_filter(self, queryset, name, value):
+        value = value + datetime.timedelta(days=1)
+        return queryset.filter(**{name: value})
 
 
 class GirlFilter(SuperFilter):
